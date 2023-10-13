16831bb232da196f9952ea243d61ee65574ac757
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2023-10-13 20:27:41 +0300

    fixed: upload file name

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
-import FileUpload from "@/components/file-upload";
+import {FileUpload} from "@/components/file-upload";
 
 import {
     Dialog,
