fec0419184db9829317344853c0f5651c3bec89b
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2023-10-15 00:55:54 +0300

    added: Video Calls

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -4,6 +4,8 @@ import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
 
+import { ChatVideoButton } from "./chat-video-button";
+
 interface ChatHeaderProps {
     serverId: string;
     name: string;
@@ -23,12 +25,18 @@ export const ChatHeader = ({
             {type === "channel" && (
                 <Hash className="w-5 h-5 mr-1 text-zinc-500 dark:text-zinc-400" />
             )}
-            {type === "conversation" && <UserAvatar src={imageUrl} className="w-8 h-8 mr-2 md:h-8 md:w-8"/>}
+            {type === "conversation" && (
+                <UserAvatar
+                    src={imageUrl}
+                    className="w-8 h-8 mr-2 md:h-8 md:w-8"
+                />
+            )}
             <p className="font-semibold text-black text-md dark:text-white">
                 {name}
             </p>
             <div className="flex items-center ml-auto">
-                <SocketIndicator/>
+                {type === "conversation" && <ChatVideoButton />}
+                <SocketIndicator />
             </div>
         </div>
     );
