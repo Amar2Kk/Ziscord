d9bbaa63738c2e56d745eec8a0fdb05aaaad217f
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2023-10-14 14:48:37 +0300

    added: Chat Messages

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -34,8 +35,10 @@ export default function RootLayout({
                     >
                         <SocketProvider>
                             <ModalProvider />
-                            <Toaster richColors position="bottom-right" />
-                            {children}
+                            <QueryProvider>
+                                <Toaster richColors position="bottom-right" />
+                                {children}
+                            </QueryProvider>
                         </SocketProvider>
                     </ThemeProvider>
                 </body>
